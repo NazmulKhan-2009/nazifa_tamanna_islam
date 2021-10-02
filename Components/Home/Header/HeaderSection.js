5563d2fe18260fc86d89c9059bd8b0c5e87eb672
NazmulKhan-2009 <nazmulustc09@gmail.com>
2021-10-02 09:51:06 +0600

    header section started

--- Components/Home/Header/HeaderSection.js
+++ Components/Home/Header/HeaderSection.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import styles from '../../../styles/HeaderSection.module.css'
-
+// import styles from '../../../styles/HeaderSection.module.css'
+// ../images/fancy_deboss.png) repeat top left;
 export default function HeaderSection() {
 
 
  return (
   <section className="" id="home">
     
-      <div className="container pt-5">
-        <div id="button">
-        
-        <div className="row">
-          <div className={`col-md-10 mr-auto  my-auto ${styles.get_appoint}`}>           
-              <h3>Lorem ipsum dolor sit amet.</h3>
-              <button className={styles.appointment_btn} >Get Appointment</button>
-        
-          </div>
-        </div>
-        
-        </div>
-      </div>
-    </section>
+      
+  </section>
  )
 }
--- Components/Home/Header/Navbar.js
+++ Components/Home/Header/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
 
     {/* <NavLink className="back-to-nav navbar-brand brand_scope" to='/'><img src={nav_logo} alt="" width="50%"/> <span>Doctor Care</span></NavLink> */}
 
-    <div className="back-to-nav navbar-brand brand_scope"><Link  href='/'><a>Nazifa Tamanna Islam</a></Link></div>
+    <div className="back-to-nav navbar-brand brand_scope"><Link  href='/'><a><span className="first_letter">N</span>AZIFA TAMANNA ISLAM</a></Link></div>
    
          
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,35 +22,27 @@ export default function Navbar() {
 
     <div  className="collapse navbar-collapse" id="navbarCollapse">
       <ul className="navbar-nav mr-auto w-100 justify-content-end clearfix">
-
-    
-        
-
-        
-
-        <li className="nav-item ">
-        
-        
-        <Link  href='/'><a>Home</a></Link>
-        </li>
-  
-       
-         
-       
+        <li className="nav-item ">       
+          <Link  href='/'><a>Who ?</a></Link>
+        </li>       
         <li className="nav-item">
-           <Link  href='/dashboard'><a>Dashboard</a></Link>
-            
-       
+           <Link  href='/dashboard'><a>Blogs</a></Link>      
+        </li>
+        <li className="nav-item">
+           <Link  href='/dashboard'><a>Gallery</a></Link>      
         </li>
 
+        <li className="nav-item">
+           <Link  href='/dashboard'><a>Social</a></Link>      
+        </li>
+        <li className="nav-item">
+           <Link  href='/dashboard'><a>YouTube</a></Link>      
+        </li>
         <li className="nav-item">
           <Link className='linkok' href="/login">
-            Log In
+            Subscribe
           </Link>
-        </li>
-
-       
-        
+        </li>              
       </ul>
     </div>
   </div>
